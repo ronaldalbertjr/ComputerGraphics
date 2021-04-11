d39d2cd1ea1b85de42d902c3600026c668b14e5e
ronald.araujo <ronald.araujo@stoneage.com.br>
2021-04-11 18:45:22 -0300

    plotting graph

--- esqueleto/t1/src/basic.renderer.js
+++ esqueleto/t1/src/basic.renderer.js
@@ -10,37 +10,129 @@
 
         
     function inside(  x, y, primitive  ) {
-        var L = [];
-        var vertices = primitive.vertices;
+        var shape = primitive.shape;
+        if(shape == 'circle'){
+            var center = primitive.center;
+            var radius = primitive.radius;
 
-        for (var i = 0; i < vertices.length; i++ ) {
-            var n = [];
+            if((x - center[0])*(x - center[0]) + (y - center[1])*(y - center[1]) > radius*radius)  return false;
 
-            if(i != vertices.length - 1) {
-                n[0] = - ( vertices[i + 1][1] - vertices[i][1] );
-                n[1] =  vertices[i + 1][0] - vertices[i][0];
-            }
-            else {
-                n[0] = - ( vertices[0][1] - vertices[i][1] );
-                n[1] =  vertices[0][0] - vertices[i][0];
-            }
-            
-            var vec_to_point = [];
-            vec_to_point[0] = x - vertices[i][0];
-            vec_to_point[1] = y - vertices[i][1];
-
-            L[i] = vec_to_point[0] * n[0] +  vec_to_point[1] * n[1];
+            return true;
         }
+        else {
+            var L = [];
+            var vertices = primitive.vertices;
 
-        const sign = L[0] > 0 ? true : false;
+            for (var i = 0; i < vertices.length; i++ ) {
+                var n = [];
 
-        for(var result of L) {
-            if(result == 0) return true;
-            if(sign != (result > 0 ? true : false)) return false;
+                if(i != vertices.length - 1) {
+                    n[0] = - ( vertices[i + 1][1] - vertices[i][1] );
+                    n[1] =  vertices[i + 1][0] - vertices[i][0];
+                }
+                else {
+                    n[0] = - ( vertices[0][1] - vertices[i][1] );
+                    n[1] =  vertices[0][0] - vertices[i][0];
+                }
+
+                var vec_to_point = [];
+                vec_to_point[0] = x - vertices[i][0];
+                vec_to_point[1] = y - vertices[i][1];
+
+                L[i] = vec_to_point[0] * n[0] +  vec_to_point[1] * n[1];
+            }
+
+            const sign = L[0] > 0 ? true : false;
+
+            for(var result of L) {
+                if(result == 0) return true;
+                if(sign != (result > 0 ? true : false)) return false;
+            }
+
+            return true;
         }
-
-        return true;
     }
+
+    function generateBoundingBox( primitive ) {
+        var shape = primitive.shape;
+        if(shape == "circle") {
+            var center = primitive.center;
+            var radius = primitive.radius;
+
+            var min_x = center[0] - radius;
+            var max_x = center[0] + radius;
+            var min_y = center[1] - radius;
+            var max_y = center[1] + radius;
+        }
+        else {
+            var vertices = primitive.vertices;
+            var min_x = vertices[0][0];
+            var max_x = vertices[0][0];
+            var min_y = vertices[0][1];
+            var max_y = vertices[0][1];
+
+            for( var i = 1; i < vertices.length; i++ ){
+                min_x = vertices[i][0] < min_x ? vertices[i][0] : min_x;
+                max_x = vertices[i][0] > max_x ? vertices[i][0] : max_x;
+                min_y = vertices[i][1] < min_y ? vertices[i][1] : min_y;
+                max_y = vertices[i][1] > max_y ? vertices[i][1] : max_y;
+            }
+        }
+
+        return {
+            min_x,
+            max_x,
+            min_y,
+            max_y,
+        }
+    }
+
+    function _multiplyMatrices( m1, m2 ) {
+        var result = [];
+        for (var i = 0; i < m1.length; i++) {
+            result[i] = [];
+            for (var j = 0; j < m2[0].length; j++) {
+                var sum = 0;
+                for (var k = 0; k < m1[0].length; k++) {
+                    sum += m1[i][k] * m2[k][j];
+                }
+                result[i][j] = sum;
+            }
+        }
+        return result;
+    }
+
+    function applyTransformation( primitive ) {
+        var vertices = primitive.vertices;
+        var xform = primitive.xform;
+        for(var i = 0; i < vertices.length; i++) {
+            vertices[i][2] = 1;
+        }
+
+        var transformedVertices = _multiplyMatrices(vertices, xform);
+
+        vertices = []
+        for(var i = 0; i < transformedVertices.length; i++) {
+            vertices[i] = []
+            vertices[i][0] = transformedVertices[i][0];
+            vertices[i][1] = transformedVertices[i][1];
+        }
+        return vertices;
+    }
+
+    function _transposeMatrix( m ) {
+        var transposedM = []
+
+        for (var i = 0; i < m.length; i++) {
+            transposedM[i] = [];
+            for (var j = 0; j < m[0].length; j++){
+                transposedM[i][j] = m[j][i];
+            }
+        }
+
+        return transposedM;
+    }
+
         
     
     function Screen( width, height, scene ) {
@@ -79,12 +171,15 @@
          
                 // In this loop, the image attribute must be updated after the rasterization procedure.
                 for( var primitive of this.scene ) {
+                    
+                    if(primitive.hasOwnProperty('xform'))
+                        primitive.vertices = applyTransformation( primitive );
 
-                    // Loop through all pixels
-                    // Use bounding boxes in order to speed up this loop
-                    for (var i = 0; i < this.width; i++) {
+                    var bounding_box = generateBoundingBox( primitive );
+
+                    for (var i = bounding_box.min_x; i <= bounding_box.max_x; i++) {
                         var x = i + 0.5;
-                        for( var j = 0; j < this.height; j++) {
+                        for( var j = bounding_box.min_y; j <= bounding_box.max_y; j++) {
                             var y = j + 0.5;
 
                             // First, we check if the pixel center is inside the primitive 
